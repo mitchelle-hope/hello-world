fbdd2f885bdbd3e2c032fa14b16a900d0f36201e
mitchelle-hope <mitchellehopewaithera@gmail.com>
2022-05-08 09:58:08 +0300

    calllog

--- app/src/main/java/com/misho/MainActivity.kt
+++ app/src/main/java/com/misho/MainActivity.kt
@@ -7,12 +7,19 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnbmi:Button
+    lateinit var btnmoney : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnbmi = findViewById(R.id.btnbmi)
+        btnmoney = findViewById(R.id.btnmoney)
         btnbmi.setOnClickListener() {
          val intent = Intent(this,bmicalculatoractivity::class.java)
+            startActivity(intent)
+        }
+
+        btnmoney.setOnClickListener {
+            val intent = Intent(this, SendMoney::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/misho/bmicalculatoractivity.kt
+++ app/src/main/java/com/misho/bmicalculatoractivity.kt
@@ -1,11 +1,21 @@
 package com.misho
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class bmicalculatoractivity : AppCompatActivity() {
+    lateinit var btnSendmoney:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmicalculatoractivity)
-    }
+        btnSendmoney = findViewById(R.id.btnSendmoney)
+        btnSendmoney.setOnClickListener {
+            val intent = Intent(this,bmicalculatoractivity::class.java)
+            startActivity(intent)
+        }
 }
+
+
+    }
